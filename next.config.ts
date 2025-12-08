66286133e82ff284b0c202595074caf792a3762b
TusharRoy23 <risenboy23@gmail.com>
2025-12-08 21:49:17 +0100

    upgraded to next 16 with react compiler support

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+  reactCompiler: true,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
   images: {
     remotePatterns: [
@@ -18,6 +20,9 @@ const nextConfig: NextConfig = {
         pathname: "/identity/images/g-logo.png",
       },
     ],
+    localPatterns: [
+      { pathname: "/images/**" }
+    ]
   },
   output: "standalone",
   env: {
